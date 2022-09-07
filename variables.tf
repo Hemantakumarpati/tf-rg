6295ccf04f6a2e3d0ac4a0d9d6a4706ad724b7b7
Hemantakumarpati <34638115+Hemantakumarpati@users.noreply.github.com>
2022-09-07 15:36:06 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "RGName" {
+    type = string
+}
+
+variable "location" {
+    type = string
+}
+
+variable "subscriptionID" {
+    type = string
+}
+
+variable "clientID" {
+    type = string
+}
+
+variable "clientsecret" {
+    type = string
+}
+
+variable "tenantID" {
+    type = string
+}
